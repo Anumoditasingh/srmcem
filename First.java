d16244f5dcac6533587434452467314cb70c8651
Anumodita <singhanumodita1997@gmail.com>
2019-02-05 00:16:26 +0530

    message first commit

---
+++ First.java
@@ -0,0 +1,29 @@
+public class First
+{
+
+public static void main(String[]args)
+
+{
+int a=7;
+
+switch(a)
+{
+case 7:System.out.println("7");
+break;
+
+case 8:System.out.println("8");
+break;
+
+case 9:System.out.println("9");
+break;
+
+default:System.out.println("default");
+break;
+
+}
+}
+}
+
+
+
+
